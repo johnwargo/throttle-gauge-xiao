89b70003e6810e84822cecf20f22fce30e94f313
johnwargo <john@johnwargo.com>
2025-03-03 19:57:09 -0500

    Update xiao-throttle-gauge.ino

--- xiao-throttle-gauge.ino
+++ xiao-throttle-gauge.ino
@@ -6,15 +6,36 @@
  ************************************************************/
 
 #include <FastLED.h>
+
 #define NUM_LEDS 7
+#define THROTTLE_PIN A0
 #define DATA_PIN 1
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("\nThrottle Gauge");
+
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+
+  if (NUM_LEDS < 5) {
+    Serial.println("Invalid Configuration");
+    Serial.println("Number of NUM_LEDS must be greater than 5.");
+    while (true) delay(100);  //loops forever
+  }
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // read the voltage from the throttle pin, returns values from 0 to 1023
+  int throttleValue = analogRead(THROTTLE_PIN);
+  // convert the voltage to a number of NeoPixels
+  int illuminatedPixels = int( do some math here);
+  // illuminate the appropriate number of NeoPixels Green
+
+  // turn the rest of the NeoPixels off
+
+  delay(10);
 }
