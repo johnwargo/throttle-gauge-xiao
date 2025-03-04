f3a4c2d543def4f9655478e1efa6cc3a1262142b
johnwargo <john@johnwargo.com>
2025-03-04 06:05:06 -0500

    Update throttle-gauge-xiao.ino

--- throttle-gauge-xiao.ino
+++ throttle-gauge-xiao.ino
@@ -12,30 +12,81 @@
 #define THROTTLE_PIN A0
 #define DATA_PIN 1
 
+// task handlers for the code running on separate processor cores
+TaskHandle_t Task0;
+TaskHandle_t Task1;
+
 CRGB leds[NUM_LEDS];
 
+int throttleValue = 0;
+int previousThrottleValue = -1;
+
 void setup() {
+
   Serial.begin(115200);
   delay(1000);
   Serial.println("\nThrottle Gauge");
+  Serial.println("By John M. Wargo\n");
+
   // initialize the FastLED library
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  // turn off all of the LEDs
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+
   // make sure we have an appropriate amount of NeoPixels to make a gauge
   if (NUM_LEDS < 5) {
     Serial.println("Invalid Configuration");
     Serial.println("Number of NUM_LEDS must be greater than 5.");
     while (true) delay(100);  //loops forever
   }
+
+  // Pin the throttle reader to processor core 0
+  xTaskCreatePinnedToCore(throttleReader, "Throttle Reader", 10240, NULL, 0, &Task0, 0);
+  // give the processor some time to get the thread running
+  delay(500);
+  // Pin the gauge updater to processor core 1
+  xTaskCreatePinnedToCore(gaugeUpdater, "Gauge Updater", 10240, NULL, 0, &Task1, 1);
+  // give the processor some time to get the thread running
+  delay(500);
 }
 
 void loop() {
-  // read the voltage from the throttle pin, returns values from 0 to 1023
-  int throttleValue = analogRead(THROTTLE_PIN);
-  // convert the voltage to a number of NeoPixels
-  int illuminatedPixels = int(do some math here);
-  // illuminate the appropriate number of NeoPixels Green
+  // nothing to do here since we have dedicated tasks
+  // running on each core looping away infinitely
+}
 
-  // turn the rest of the NeoPixels off
+void throttleReader(void* pvParameters) {
 
-  delay(10);
+  Serial.print("Throttle Reader running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for (;;) {  // an infinite loop
+    // read the voltage from the throttle pin, returns values from 0 to 1023
+    throttleValue = analogRead(THROTTLE_PIN);
+    // throw in a little delay for the ESP32 watchdog to do its cleanup
+    delay(25);
+  }
+}
+
+void gaugeUpdater(void* pvParameters) {
+
+  Serial.print("Gauge Updater running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for (;;) {  // an infinite loop
+    // Did the throttle value change?
+    if (throttleValue != previousThrottleValue) {
+      // reset the previous throttle value
+      previousThrottleValue = throttleValue;
+      // update the gauge
+      // convert the voltage to a number of NeoPixels
+      int illuminatedPixels = int(throttleValue / 1023);
+      // turn off all of the LEDs; this may happen fast enough to not be visible
+      fill_solid(leds, NUM_LEDS, CRGB::Black);
+      // light the green ones based on the throttle value
+      fill_solid(leds, throttleValue, CRGB::Green);
+    }
+    // throw in a little delay for the ESP32 watchdog to do its cleanup
+    delay(25);
+  }
 }
