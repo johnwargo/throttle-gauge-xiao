57424863b27d895cd31a9c804d50c13a1ca01892
johnwargo <john@johnwargo.com>
2025-03-03 19:40:48 -0500

    coding away

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Throttle Gauge (XAIO)
+
+Built using a Seeed Studio XIAO C6 board and a bunch of NeoPixels.
+
+Reads voltage (0 to 15V) on pin `A0`
+
+Drives NeoPixels on pin `D1`
+
+## Reference
+
+[Voltage Divider](https://ohmslawcalculator.com/voltage-divider-calculator)
+
+![Voltage Divider Calculator](images/voltage-divider-calculator.png)
+
--- xiao-throttle-gauge.ino
+++ xiao-throttle-gauge.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+/************************************************************
+ * Xiao Throttle Gauge
+ * By John M. Wargo
+ * https://johnwargo.com
+ *
+ ************************************************************/
 
+#include <FastLED.h>
+#define NUM_LEDS 7
+#define DATA_PIN 1
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
